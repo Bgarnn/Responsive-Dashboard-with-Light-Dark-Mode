0d7b4cb04b4e12fc01b1cd63932e11099818bf02
Karn Buralek <karn.buralek904@gmail.com>
2024-06-20 09:37:02 +0700

    dash

---
+++ Dashboard-with-dark-light-mode/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+	{
+		productName: 'Foldable Mini Drone',
+		productNumber: '85631',
+		PaymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+	{
+		productName: 'Foldable Mini Drone',
+		productNumber: '85631',
+		PaymentStatus: 'Due',
+		shipping: 'Declined',
+	},
+	{
+		productName: 'Foldable Mini Drone',
+		productNumber: '85631',
+		PaymentStatus: 'Due',
+		shipping: 'Delivered',
+	},
+	{
+		productName: 'Foldable Mini Drone',
+		productNumber: '85631',
+		PaymentStatus: 'Due',
+		shipping: 'Pending',
+	},
+	{
+		productName: 'Foldable Mini Drone',
+		productNumber: '85631',
+		PaymentStatus: 'Due',
+		shipping: 'Pending',
+	}
+]
